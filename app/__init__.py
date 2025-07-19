78d987c001337a4357d5560638e221e359993c8d
ijromerob <ijromerob@gmail.com>
2025-07-19 15:08:44 -0600

    added cors to the backend

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from .routes import register_blueprints
 from .config import Config
 from flasgger import Swagger
+from flask_cors import CORS
 
 
 def create_app():
 
     app = Flask(__name__)
 
-    swagger = Swagger(app)
+    Swagger(app)
 
     app.config.from_object(Config)
+
+    CORS(app, origins=["http://localhost:3000"])
 
     register_blueprints(app)
 
