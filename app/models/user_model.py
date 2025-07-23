67aac62994d7c3ffe9ea14e522294f2b8c555146
ijromerob <ijromerob@gmail.com>
2025-07-23 09:34:32 -0600

    added login route

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -48,3 +48,51 @@ def create_user(data):
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+LOGIN_USER = """
+SELECT user_id, password_hash, account_type, first_name, last_name, company
+FROM Users
+WHERE email = %s;
+"""
+
+
+def log_in_user(data):
+    email = data["email"]
+    password = data["password"]
+
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    LOGIN_USER,
+                    (email,),
+                )
+                user = cursor.fetchone()
+        if not user:
+            return jsonify({"error": "Invalid email or password"}), 401
+
+        user_id, stored_hash, account_type, first_name, last_name, company = user
+
+        if bcrypt.checkpw(password.encode("utf-8"), stored_hash.encode("utf-8")):
+            return (
+                jsonify(
+                    {
+                        "message": "Login successful",
+                        "user": {
+                            "user_id": user_id,
+                            "account_type": account_type,
+                            "first_name": first_name,
+                            "last_name": last_name,
+                            "company": company,
+                            "email": email,
+                        },
+                    }
+                ),
+                200,
+            )
+        else:
+            return jsonify({"error": "Invalid email or password"}), 401
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
