b4a8836998094449e11190e17732f6fe87d67d7d
ijromerob <ijromerob@gmail.com>
2025-07-26 15:12:16 -0600

    added the needed parts route to get a view from all work orders

--- app/routes/parts.py
+++ app/routes/parts.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from app.models.parts_model import get_all_products
+from app.models.parts_model import get_all_products, get_needed_parts
 
 parts_bp = Blueprint("parts", __name__)
 
@@ -32,4 +32,44 @@ def get_products():
         description: Server error
     """
     response = get_all_products()
+    return response
+
+
+@parts_bp.get("/needed_parts")
+def get_parts_needed():
+    """
+    Get All Needed Parts
+    ---
+    tags:
+      - Parts
+    summary: Retrieve all parts needed for all work orders
+    responses:
+      200:
+        description: Report generated successfully
+        content:
+          application/json:
+            schema:
+              type: array
+              items:
+                type: object
+                properties:
+                  work_order:
+                    type: string
+                    example: "WO0000001"
+                  part_number:
+                    type: string
+                    example: "P-12345"
+                  description:
+                    type: string
+                    example: "Widget Housing"
+                  quantity_required:
+                    type: integer
+                    example: 100
+                  quantity_supplied:
+                    type: integer
+                    example: 75
+      500:
+        description: Internal server error
+    """
+    response = get_needed_parts()
     return response
